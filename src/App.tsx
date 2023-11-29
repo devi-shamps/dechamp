b7091abcaabdf4c2e9d87b8398564756ea0215b7
Dimitri <dimitri.dechamp@gmail.com>
2023-11-29 16:14:30 +0100

    V1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {FunctionComponent} from 'react'
+import './App.css'
+
+const App: FunctionComponent = () => {
+
+  return (
+    <div>
+      <h1>Site web du con</h1>
+      <iframe src="https://hamlayes.github.io/hamlaoui-portfolio/"width="1080" height="720" ></iframe>
+    </div>
+  )
+}
+
+export default App
